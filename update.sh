14be1c79d02b91d5eddc6ff59ecd9ff15bef3242
Matthew Merkas <matthewjm99@gmail.com>
2024-04-12 03:15:02 +1000

    Copy static files to web root

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@
 export NODE_OPTIONS=--max_old_space_size=4096
 systemctl stop docker httpd mongod
 ng build --configuration=production
+cp ./dist/merkas-web /var/www/html
 systemctl start docker httpd mongod
