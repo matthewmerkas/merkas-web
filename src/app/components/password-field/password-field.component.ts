7c1cc0f499a1beac247cd7b2a88fbec0fe6f41be
Matthew Merkas <matthewjm99@gmail.com>
2024-05-15 02:33:37 +1000

    Added/improved tooltips

--- src/app/components/password-field/password-field.component.ts
+++ src/app/components/password-field/password-field.component.ts
@@ -5,6 +5,8 @@ import { MatIconButton } from '@angular/material/button'
 import { MatError, MatFormField } from '@angular/material/form-field'
 import { MatIcon } from '@angular/material/icon'
 import { MatInput, MatLabel, MatSuffix } from '@angular/material/input'
+import { TOOLTIP_DELAY } from '../../functions/constants'
+import { MatTooltip } from '@angular/material/tooltip'
 
 @Component({
   selector: 'app-password-field',
@@ -18,7 +20,8 @@ import { MatInput, MatLabel, MatSuffix } from '@angular/material/input'
     MatLabel,
     MatSuffix,
     NgIf,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatTooltip
   ],
   templateUrl: './password-field.component.html',
   styleUrl: './password-field.component.scss'
@@ -27,4 +30,10 @@ export class PasswordFieldComponent {
   @Input() fc: FormControl = new FormControl<string>('')
   @Input() label: string = 'Password'
   showPassword = false
+  tooltip = 'Show ' + this.label
+  protected readonly TOOLTIP_DELAY = TOOLTIP_DELAY
+
+  onMouseEnter = () => {
+    this.tooltip = (this.showPassword ? 'Hide ' : 'Show ') + this.label
+  }
 }
