9040e05be1aa783c043ef8d71256f3c9f7e8a876
Matthew Merkas <matthewjm99@gmail.com>
2024-05-15 02:10:10 +1000

    Run `npm install` also

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 git pull
 export NODE_OPTIONS=--max_old_space_size=4096
 systemctl stop docker httpd mongod
+npm install
 ng build --configuration=production
 rm -rf /var/www/html
 cp -r ./dist/merkas-web /var/www/html
