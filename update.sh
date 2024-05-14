c0c68edfb7424696a2c3b935057abb09096467f9
Matthew Merkas <matthewjm99@gmail.com>
2024-05-15 02:14:09 +1000

    Allow script to be run by non-root user

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 git pull
 export NODE_OPTIONS=--max_old_space_size=4096
-systemctl stop docker httpd mongod
+sudo systemctl stop docker httpd mongod
 npm install
 ng build --configuration=production
-rm -rf /var/www/html
-cp -r ./dist/merkas-web /var/www/html
-systemctl start docker httpd mongod
+sudo rm -rf /var/www/html
+sudo cp -r ./dist/merkas-web /var/www/html
+sudo systemctl start docker httpd mongod
