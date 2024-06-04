af87ecfcba0a56e5b39b34cbc16d48df090d8023
Matthew Merkas <matthewjm99@gmail.com>
2024-06-04 22:20:21 +1000

    Make update script output more verbose

--- update.sh
+++ update.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
-git pull
 export NODE_OPTIONS=--max_old_space_size=4096
+echo Updating source...
+git pull
+echo Stopping services... \(docker, apache, mongo\)
 sudo systemctl stop docker httpd mongod
+echo Installing npm packages...
 npm install
+echo Building Angular application...
 ng build --configuration=production
+echo Copying static files...
 sudo rm -rf /var/www/html
 sudo cp -r ./dist/merkas-web /var/www/html
+echo Starting services...
 sudo systemctl start docker httpd mongod
