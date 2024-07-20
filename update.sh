7c32d9e57d079b12646a81d16f1013acfd9e07da
Matthew Merkas <matthewjm99@gmail.com>
2024-07-20 22:51:33 +0100

    Update submodules, too

--- update.sh
+++ update.sh
@@ -3,7 +3,8 @@
 cd "$(dirname "$0")" || exit
 export NODE_OPTIONS=--max_old_space_size=4096
 echo Updating source...
-git pull
+git submodule update --init --recursive
+git pull --recurse-submodules  && git submodule update --recursive
 echo Stopping services... \(docker, apache, mongo\)
 sudo systemctl stop docker httpd mongod
 echo Installing npm packages...
