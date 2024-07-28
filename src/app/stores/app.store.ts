9d000170d898909e92a4aa2dd42293770b3daef4
Matthew Merkas <matthewjm99@gmail.com>
2024-07-28 21:05:05 +1000

    Added File Converter & Ping Plotter shortcuts. Redesigned app icons

--- src/app/stores/app.store.ts
+++ src/app/stores/app.store.ts
@@ -6,19 +6,31 @@ import { getToken } from '../functions/local-storage'
 export class AppStore {
   @observable array: App[] = [
     {
+      title: 'File Converter',
+      subtitle: 'Convert any file locally',
+      imageUrl: '/assets/icons/apps/light/converter-touch.svg',
+      routerLink: '/converter'
+    },
+    {
+      title: 'Ping Plotter',
+      subtitle: 'Record network latency',
+      imageUrl: '/assets/icons/apps/light/plotter-touch.svg',
+      routerLink: '/plotter'
+    },
+    {
       title: 'Twenty',
-      subtitle: '20-20-20 timer',
-      imageUrl: '/assets/icons/twenty-touch.svg',
+      subtitle: '20-20-20 eye care timer',
+      imageUrl: '/assets/icons/apps/light/twenty-touch.svg',
       routerLink: '/twenty'
     }
   ]
 
   constructor() {
     if (getToken()) {
-      this.array.splice(0, 0, {
+      this.array.splice(-1, 0, {
         title: 'Sites',
         subtitle: 'Text & file sharing, links',
-        imageUrl: '/assets/icons/sites-touch.svg',
+        imageUrl: '/assets/icons/apps/light/sites-touch.svg',
         routerLink: '/sites'
       })
     }
