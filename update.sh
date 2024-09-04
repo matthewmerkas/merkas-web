7fc02fe1de343e68cd931c0eb0b5b5eccf71ccf6
Matthew Merkas <matthewjm99@gmail.com>
2024-09-05 00:57:44 +1000

    Don't stop apache or mongo. Restart merkas-web service

--- update.sh
+++ update.sh
@@ -5,15 +5,11 @@ export NODE_OPTIONS=--max_old_space_size=4096
 echo Updating source...
 git submodule update --init --recursive
 git pull --recurse-submodules  && git submodule update --recursive
-echo Stopping services... \(docker, apache, mongo\)
-sudo systemctl stop docker httpd mongod
+echo Stopping services... \(docker\)
+sudo systemctl stop docker
 echo Installing npm packages...
 sudo npm install
 echo Building Angular application...
 ng build --configuration=production
-echo Copying static files...
-sudo rm -rf /var/www/html
-sudo cp -r ./dist/merkas-web/browser /var/www/html
-echo Starting services...
-sudo systemctl restart merkas
-sudo systemctl start docker httpd mongod
+echo \(Re\)starting services...
+sudo systemctl restart docker merkas merkas-web
