32e9aef093ce567e7e09594bb32401b3db0d9b0b
Matthew Merkas <matthewjm99@gmail.com>
2024-09-19 17:03:45 +1000

    Added close button to apps dialog on mobile

--- src/app/components/apps-dialog/apps-dialog.component.ts
+++ src/app/components/apps-dialog/apps-dialog.component.ts
@@ -1,12 +1,26 @@
-import { NgForOf } from '@angular/common'
+import { NgForOf, NgIf } from '@angular/common'
 import { Component } from '@angular/core'
 import { Store } from '../../stores/store'
 import { SiteComponent } from '../site/site.component'
+import { TOOLTIP_DELAY } from '../../functions/constants'
+import { MatButton, MatFabButton } from '@angular/material/button'
+import { MatIcon } from '@angular/material/icon'
+import { MatTooltip } from '@angular/material/tooltip'
+import { MatDialogClose } from '@angular/material/dialog'
 
 @Component({
   selector: 'app-apps-dialog',
   standalone: true,
-  imports: [NgForOf, SiteComponent],
+  imports: [
+    NgForOf,
+    SiteComponent,
+    MatFabButton,
+    MatIcon,
+    MatTooltip,
+    NgIf,
+    MatDialogClose,
+    MatButton
+  ],
   templateUrl: './apps-dialog.component.html',
   styleUrl: './apps-dialog.component.scss'
 })
@@ -19,4 +33,5 @@ export class AppsDialogComponent {
       buttonLabel: ''
     }
   }
+  protected readonly TOOLTIP_DELAY = TOOLTIP_DELAY
 }
